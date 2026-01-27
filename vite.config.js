048db7dbd0a87d3524055fce7fc27e0875ab2a78
AbdedjalilKaci <a_kaci@estin.dz>
2026-01-27 13:08:18 +0000

    fixed

--- vite.config.js
+++ vite.config.js
@@ -22,10 +22,10 @@ export default defineConfig(({ mode }) => {
     server: {
       host: true,
       port: 5173,
-      allowedHosts: [
-        'localhost',
-        '127.0.0.1'
-      ]
+      allowedHosts: ['.railway.app', 'dz-kitab-frontend-production.up.railway.app']
+    },
+    preview: {
+      allowedHosts: ['.railway.app', 'dz-kitab-frontend-production.up.railway.app']
     }
   }
 })
